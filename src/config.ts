8bd2015cbb1a070772a95c9ce2b396dc8c8f0047
betty2310 <huynh.dh2310@gmail.com>
2026-02-16 11:03:30 +0700

    inital commit

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://betty2310.com/", // replace this with your deployed domain
+  author: "betty2310",
+  profile: "https://betty2310.com/about/",
+  desc: "Dương Hữu Huynh homepage. Focus about Tech, Development, research",
+  title: "betty2310's home",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -12,9 +12,9 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/betty2310/home/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
